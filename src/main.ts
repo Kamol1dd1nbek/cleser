b6fbb99dcacc019abe62e81150c64c53a5cd3741
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-04 08:39:29 +0500

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const start = async () => {
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+    .setTitle("CleSer")
+    .setDescription("Mini project for cleaning service")
+    .addTag("NodeJs, Nestjs, Postgres, Sequelize, JWT, OTP, Bot, Swagger")
+    .setVersion("1.0.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("cleser/docs", app, document);
+    const PORT = process.env.PORT || 3030;
+    
+    app.use(cookieParser());
+    app.setGlobalPrefix("api");
+    app.useGlobalPipes(new ValidationPipe());
+
+    await app.listen(PORT, () => {
+      console.log(`Servert is running on port: ${PORT}`);
+    })
+  } catch (error) {
+    console.log(error);    
+  }
 }
-bootstrap();
+start();
