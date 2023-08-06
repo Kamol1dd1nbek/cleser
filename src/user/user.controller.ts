bb7d9c171f3b910b12d8e9b504ae257637c3db16
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-06 08:31:42 +0500

    User: delete, update, find (one | all)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,28 +2,35 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { RegistrationUserDto } from './dto/registration-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+
+@ApiTags("Users")
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // @Get()
-  // findAll() {
-  //   return this.userService.findAll();
-  // }
+  @ApiOperation({ summary: '| Find all users' })
+  @Get()
+  findAllUsers() {
+    return this.userService.findAllUsers();
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.userService.findOne(+id);
-  // }
+  @ApiOperation({ summary: "| Find user by id" })
+  @Get(':id')
+  findOneUser(@Param('id') id: string) {
+    return this.userService.findOneUser(+id);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.userService.update(+id, updateUserDto);
-  // }
+  @ApiOperation({ summary: "| Update user" })
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.userService.remove(+id);
-  // }
+  @ApiOperation({ summary: "| Delete user" })
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { RegistrationUserDto } from './dto/registration-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './models/user.model';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Role } from '../role/models/role.model';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User) private readonly userRepo: typeof User) {}
 
-  @ApiOperation({ summary: "| Find all users" })
-  async findAllUsers() {
+  async findAllUsers(): Promise<User[]> {
     const users = await this.userRepo.findAll({ include: { all: true } });
+
+    if (users.length == 0) {
+      throw new BadRequestException('No users have been added yet');
+    }
+
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOneUser(id: number): Promise<User> {
+    const user = await this.userRepo.findOne({ where: { id },include: { all: true } });
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    
+    if (updateUserDto.password) {
+      const hashed_password = await bcrypt.hash(updateUserDto.password, 10);
+      const updatedUser = await this.userRepo.update({...updateUserDto, hashed_password}, { where: { id }, returning: true });
+      if ( updatedUser[0] == 0 ) throw new BadRequestException("User not found");
+      else return updatedUser[1][0].dataValues;
+    } else {
+      const updatedUser = await this.userRepo.update(updateUserDto, { where: { id }, returning: true });
+      if ( updatedUser[0] == 0 ) throw new BadRequestException("User not found");
+      else return updatedUser[1][0].dataValues;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const isDeleted = await this.userRepo.destroy({ where: { id } });
+
+    if (isDeleted) {
+      return {
+        message: "Successfully deleted",
+        status: 200
+      }
+    } else {
+      throw new BadRequestException(`User not found with id: ${id}`)
+    }
   }
 }
