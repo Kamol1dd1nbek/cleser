902fcde6049c332f2408c3125df988da7355526d
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-07 08:41:11 +0500

    Booking

--- src/booking/booking.model.ts
+++ src/booking/booking.model.ts
@@ -5,36 +5,27 @@ import { UpdateBookingDto } from './dto/update-booking.dto';
 import { JwtAuthGuard } from '../guards/JwtAuth.guard';
 import { RolesGuard } from '../guards/RolesAuth.guard';
 import { Roles } from '../decorators/roles-auth.decorator';
+import { CookieGetter } from '../decorators/cookieGetter.decorator';
 
 @Controller('booking')
 export class BookingController {
   constructor(private readonly bookingService: BookingService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Roles("ADMIN", "USER")
   @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createBookingDto: CreateBookingDto) {
     return this.bookingService.create(createBookingDto);
   }
 
+  @Roles("ADMIN", "WORKER")
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
-    return this.bookingService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.bookingService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookingDto: UpdateBookingDto) {
-    return this.bookingService.update(+id, updateBookingDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bookingService.remove(+id);
+  findAll(
+    @CookieGetter("refresh_token") refreshToken: string
+  ) {
+    return this.bookingService.findAll(refreshToken);
   }
 }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,30 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Booking } from './models/booking.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class BookingService {
-  constructor(@InjectModel(Booking) private readonly bookingRepo: typeof Booking) {}
+  constructor(
+    @InjectModel(Booking) private readonly bookingRepo: typeof Booking,
+    private readonly jwtService: JwtService) {}
 
   create(createBookingDto: CreateBookingDto) {
     return this.bookingRepo.create(createBookingDto);
   }
 
-  findAll() {
-    return `This action returns all booking`;
+  async findAll(refreshToken: string) {
+    const userData = await this.jwtService.verify(refreshToken, {
+      secret: process.env.REFRESH_TOKEN_KEY,
+    });
+
+    if (!userData) {
+      throw new BadRequestException("Unauthorization")
+    }
+
+    if (userData.roles[0].name !== "WORKER") {
+      throw new BadRequestException("You are not worker")
+    }
+    return this.bookingRepo.findAll({ where: {worker_id: userData.id} });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} booking`;
-  }
-
-  update(id: number, updateBookingDto: UpdateBookingDto) {
-    return `This action updates a #${id} booking`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} booking`;
-  }
 }
